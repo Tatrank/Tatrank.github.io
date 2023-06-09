1b8f762342cf2831ccba52f540c1bfa8eed8e6e9
Tatranka <113363195+Tatrank@users.noreply.github.com>
2023-06-09 16:50:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let video = document.querySelector("video");
 let constraints = {
   video: {
     facingMode: "user",
-    width: 500,
-    height: 800,
+    width: 300,
+    height: 480,
   },
 };
 let button1 = document.getElementById("Detection");
